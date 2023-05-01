7680ac651be2de4601e40614709a8dd6738c4216
Ibrahim60 <49660155+Ibrahim60@users.noreply.github.com>
2023-05-01 13:55:06 +0500

    BarChart Added - stackblitz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 
+import BarChart from './components/BarChart'
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 className="intro">D3</h1>
+      <BarChart/>
     </div>
   );
 }
